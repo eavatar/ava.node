6163bdef39bc5a6cc27b2f84e3bdea320416ec70
sampot <sam@eavatar.com>
2015-05-06 20:56:33 +0800

    added a trick to set default encoding to utf-8

--- ava/core/agent.py
+++ ava/core/agent.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, unicode_literals
+
+# hack for setting default encoding!
+import sys
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
 
 import gevent
 from gevent import monkey
